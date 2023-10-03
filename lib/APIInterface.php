c6c94a549f8ec409754b40d0a1b7194a63821f2d
Chris van den Berg <chrizus@gmail.com>
2023-10-03 23:44:00 +0200

    Update APIInterface.php

--- lib/APIInterface.php
+++ lib/APIInterface.php
@@ -2,10 +2,9 @@
 
 namespace IMVDb;
 
-interface APIInterface
-{
-	public function searchVideos($query='', $start=0, $limit=10);
-	public function searchEntities($query='', $start=0, $limit=10);
-	public function video($id=0, $includes=array('sources'));
-	public function entity($id=0, $includes=array(''));
+interface APIInterface {
+	public function searchVideos ($query = "", $start = 0, $limit = 10);
+	public function searchEntities ($query = "", $start = 0, $limit = 10);
+	public function video ($id = 0, $includes = array("sources"));
+	public function entity ($id = 0, $includes = array(""));
 }
